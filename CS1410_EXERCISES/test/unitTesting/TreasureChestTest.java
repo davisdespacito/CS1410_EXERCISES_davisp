e484a5a3a89aa7308bdc1f050b50d504ba6bec3a
davisp <davisp@localhost>
2026-02-15 16:34:03 -0700

    updated jUnit tests, 4:33pm (vid @ 34:15)

--- CS1410_EXERCISES/test/unitTesting/TreasureChestTest.java
+++ CS1410_EXERCISES/test/unitTesting/TreasureChestTest.java
@@ -1,5 +1,6 @@
 package unitTesting;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -9,7 +10,7 @@ class TreasureChestTest {
 
 	private TreasureChest treasure;
 	private TreasureChest emptyChest;
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
 		treasure = new TreasureChest(10);
@@ -17,10 +18,26 @@ class TreasureChestTest {
 	}
 
 	@Test
-	void testTreasureChest() {
-		fail("Not yet implemented");
+	void treasureChest_PositiveNumberOfCoins_AllCoinsStoredInTreasureChest() {
+		TreasureChest treasure7 = new TreasureChest(7);
+		int expected = 7;
+		int actual = treasure7.getGold();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void treasureChest_ZeroCoins_NoCoinsStoredInTreasureChest() {
+		TreasureChest treasure0 = new TreasureChest(0);
+		int expected = 0;
+		int actual = treasure0.getGold();
+		assertEquals(expected, actual);
 	}
 
+	@Test
+	void treasureChest_NegativeNumberOfCoins_ThrowsIllegalArgumentException() {
+		assertThrows(IllegalArgumentException.class, () -> {new TreasureChest(-2);});
+	}
+	
 	@Test
 	void testAddGold() {
 		fail("Not yet implemented");
@@ -41,13 +58,13 @@ class TreasureChestTest {
 		String expected = "[ 10 coins ]";
 		String actual = treasure.toString();
 		assertEquals(expected, actual);
-		}
-	
+	}
+
 	@Test
 	void toString_NoCoins_ReturnNumberCoinsInBracket() {
 		String expected = "[ 0 coins ]";
 		String actual = emptyChest.toString();
 		assertEquals(expected, actual);
-		}
+	}
 
 }
