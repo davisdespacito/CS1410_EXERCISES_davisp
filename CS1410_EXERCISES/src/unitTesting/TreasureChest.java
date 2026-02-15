7d94a5b58324389c13fd39187cf7942ac9beac37
davisp <davisp@localhost>
2026-02-15 15:14:17 -0700

    progress at 3:14pm

--- CS1410_EXERCISES/src/unitTesting/TreasureChest.java
+++ CS1410_EXERCISES/src/unitTesting/TreasureChest.java
@@ -1,7 +1,61 @@
 package unitTesting;
 
+import java.math.BigDecimal;
+
+
+/**
+ * Treasure Chest that is defined by the amount of gold coins that are in 
+ * it. In this class we assume that each coin is a twenty-dollar Liberty 
+ * Gold Coin. This assumption combined with the knowledge of the gold price 
+ * and the amount of gold in each coin allows us to calculate the dollar
+ * value of the treasure chest.
+ * 
+ * @author davisp
+ */
 public class TreasureChest {
 
+	
+	
+	private int gold;
+	// private static final BigDecimal GOLD_PER_COIN = ; must be initialized
+
+	/**
+	 * Amount of gold per coin measured in ounces
+	 */
+	private static BigDecimal goldPrice;
+	
+	
+	/**
+	 * 
+	 * @param gold
+	 */
+	public TreasureChest(int goldCoins) {
+		
+		this.gold = goldCoins;
+		
+		
+	}
+	
+	/**
+	 * 
+	 * @return the gold
+	 */
+	public int getGold() {
+		
+		return gold;
+		
+	}
+	
+	/**
+	 * 
+	 * @param numberOfCoins
+	 */
+	public void addGold(int numberOfCoins) {
+		
+		
+		
+	}
+	
 	
 	
 }
